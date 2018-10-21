a4a1834e9372a7153be24fdff834fc6a78516983
saiki-iijima <saiiijim@yahoo-corp.jp>
2018-10-21 18:22:59 +0900

    cell入れてまだビルドできる

---
+++ ios/xcode/MoeGitHubSample/RepositoryListViewController.h
@@ -0,0 +1,13 @@
+//
+//  RepositoryListViewController.h
+//  MoeGitHubSample
+//
+//  Created by 飯島　彩輝 on 2018/10/21.
+//  Copyright © 2018年 saiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RepositoryListViewController : UIViewController
+
+@end
---
+++ ios/xcode/MoeGitHubSample/RepositoryListViewController.m
@@ -0,0 +1,37 @@
+//
+//  RepositoryListViewController.m
+//  MoeGitHubSample
+//
+//  Created by 飯島　彩輝 on 2018/10/21.
+//  Copyright © 2018年 saiki. All rights reserved.
+//
+
+#import "RepositoryListViewController.h"
+
+@interface RepositoryListViewController ()
+
+@end
+
+@implementation RepositoryListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
